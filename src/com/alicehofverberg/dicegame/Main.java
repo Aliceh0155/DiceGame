de727d62beade95390f32bf4331072534b22ae60
aliceh0155 <hofverbergalice@gmail.com>
2023-09-27 19:45:09 +0200

    making progress

--- src/com/alicehofverberg/dicegame/Main.java
+++ src/com/alicehofverberg/dicegame/Main.java
@@ -1,5 +1,6 @@
 package com.alicehofverberg.dicegame;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,9 +9,36 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
 
-        System.out.println("Name of player1? ");
+        System.out.println("Welcome, this is a game for 2 players!");
+        System.out.println("Input the name of Player 1! ");
         Player player1 = new Player(sc.nextLine());
-        System.out.println("player 1 is: " + player1.playerName);
+        System.out.println("Player 1 is:  " + player1.playerName);
+
+        System.out.println("Input the name of Player 2! ");
+        Player player2 = new Player(sc.nextLine());
+        System.out.println("Player 2 is:  " + player2.playerName);
+
+
+
+        System.out.println("How many dice would you like to throw?");
+        int rounds = sc.nextInt();
+        for (int i = 0; i< rounds; i++ ){
+
+            System.out.println(dieRoll());
+        }
+
+
+
 
     }
+
+    public static int dieRoll (){ //method with a die roll
+
+        Random random = new Random();
+        return random.nextInt(6)+1; //the span is 1-6
+
+
+    }
+
 }
+
